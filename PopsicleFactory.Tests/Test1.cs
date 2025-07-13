c536faa8398bff8e152526faebb74dbdb4a5df89
Kamiethenerd <kamiethenerd@gmail.com>
2025-07-13 15:04:29 -0500

    Add MSTest configuration and initial test for PopsicleController

---
+++ PopsicleFactory.Tests/Test1.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using API.Controllers;
+using API;
+
+[TestClass]
+public class PopsicleControllerTests
+{
+    [TestMethod]
+    public void CreatePopsicle_ReturnsConflict_WhenDuplicate()
+    {
+        // Arrange
+        var controller = new PopsicleController();
+        var popsicle = new Popsicle { Name = "Test", Flavor = "Grape" };
+
+        // Act
+        var result1 = controller.CreatePopsicle(popsicle);
+        var result2 = controller.CreatePopsicle(popsicle);
+
+        // Assert
+        Assert.IsInstanceOfType(result2, typeof(ConflictObjectResult));
+    }
+}
